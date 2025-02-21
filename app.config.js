bb36993bbf537e53f169f37cebb8291b99b12d81
YashWadhvani <yashwadhvani123@gmail.com>
2025-02-21 22:01:48 +0530

    iOS and Android config Updated

--- app.config.js
+++ app.config.js
@@ -1,42 +1,40 @@
 export default {
-    "expo": {
-      "name": "NeoBin_FrontEnd",
-      "slug": "NeoBin_FrontEnd",
-      "version": "1.0.0",
-      "orientation": "portrait",
-      "icon": "./assets/Logo.png",
-      "userInterfaceStyle": "light",
-      "newArchEnabled": true,
-      "splash": {
-        "image": "./assets/Logo.png",
-        "resizeMode": "contain",
-        "backgroundColor": "#FFF8EA"
-      },
-      "ios": {
-        "supportsTablet": true,
-        "bundleIdentifier": "com.yourcompany.yourapp"
-      },
-      "android": {
-        "adaptiveIcon": {
-          "foregroundImage": "./assets/Logo.png",
-          "backgroundColor": "#FFF8EA"
+    expo: {
+        name: "NeoBin",
+        slug: "NeoBin",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/Logo.png",
+        userInterfaceStyle: "light",
+        newArchEnabled: true,
+        splash: {
+            image: "./assets/Logo.png",
+            resizeMode: "contain",
+            backgroundColor: "#FFF8EA",
         },
-        "package": "com.yashwadhvani.NeoBin_FrontEnd"
-      },
-      "web": {
-        "favicon": "./assets/Logo.png"
-      },
-      "plugins": [
-        "expo-font"
-      ],
-      "assetBundlePatterns": [
-        "**/*"
-      ],
-      "extra": {
-        "eas": {
-          "projectId": "e2ea6d1c-a996-4a12-a798-0d1031b8bcf1"
-        }
-      }
-    }
-  }
-  
+        ios: {
+            supportsTablet: true,
+            bundleIdentifier: "com.yashwadhvani.NeoBin",
+            infoPlist: {
+                ITSAppUsesNonExemptEncryption: false,
+            },
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/Logo.png",
+                backgroundColor: "#FFF8EA",
+            },
+            package: "com.yashwadhvani.NeoBin",
+        },
+        web: {
+            favicon: "./assets/Logo.png",
+        },
+        plugins: ["expo-font"],
+        assetBundlePatterns: ["**/*"],
+        extra: {
+            eas: {
+                projectId: "e2ea6d1c-a996-4a12-a798-0d1031b8bcf1",
+            },
+        },
+    },
+};
